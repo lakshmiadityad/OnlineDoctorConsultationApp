dcd5f0c2a81a5df2d21e98fe647ab0c903bc36e5
Devarakonda Lakshmi Aditya <Lakshmiaditya.D@sonata-software.com>
2022-02-22 18:56:01 +0530

    changes for errors

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { AlertService, UserService, AuthenticationService } from '../_services';
 
+
 @Component({ templateUrl: 'register.component.html' })
 export class RegisterComponent implements OnInit {
     registerForm!: FormGroup;
